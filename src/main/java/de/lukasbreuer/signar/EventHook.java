e34b2798c72d5000ee0211a4ad56dbe99da11c17
Lukas <contact@lukasbreuer.de>
2025-10-18 12:58:40 +0200

    Implement EventHook

---
+++ src/main/java/de/lukasbreuer/signar/EventHook.java
@@ -0,0 +1,17 @@
+package de.lukasbreuer.signar;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface EventHook {
+  /**
+   * The call priority of the hook
+   * (determines the call sequence, relative to the other hooks)
+   * @return The priority
+   */
+  HookPriority priority() default HookPriority.NEUTRAL;
+}
